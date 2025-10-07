46df744f12c93d9cd17409245fa3b159fb9dfdfd
lincetic <eddie@lincetic.es>
2025-10-07 12:47:10 +0200

    tasks 2-9

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,22 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Check if both username and password are provided
+  if (username && password) {
+      // Check if the user does not already exist
+      if (!isValid(username)) {
+          // Add the new user to the users array
+          users.push({"username": username, "password": password});
+          return res.status(200).json({message: "User successfully registered. Now you can login"});
+      } else {
+          return res.status(404).json({message: "User already exists!"});
+      }
+  }
+  // Return error if username or password is missing
+  return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
@@ -18,26 +32,38 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+
+  return res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author;
+  let filter_author=  Object.values(books).filter(book=> book.author.toLowerCase() === author.toLowerCase());
+  return res.send(filter_author);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let title = req.params.title;
+  let filter_title=  Object.values(books).filter(book=> book.title.toLowerCase() === title.toLowerCase());
+  return res.send(filter_title);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  if(!books[isbn]){
+    return res.send("ISBN not found");
+  }
+  let reviews = books[req.params.isbn].reviews;
+  if(reviews.length>0){
+    return res.send(reviews);   
+  }else{
+    return res.send("No reviews found for the given ISBN");
+  }
+
 });
 
 module.exports.general = public_users;
